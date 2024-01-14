681be0027ed9943a285cc85441b7f65966f84c70
wzj998 <>
2024-01-15 00:49:07 +0800

    adjust overlays position

--- PythonProject/AboutDraw/OverlayImgTool.py
+++ PythonProject/AboutDraw/OverlayImgTool.py
@@ -21,11 +21,11 @@ def draw_overlays_on_img(img: Image, row: pd.Series, power_level_min: int, power
 def draw_left_top(draw, font, row, x_ratio, y_ratio, _):
     # show lap
     lap = row[COL_NAME_LAP]
-    draw_text(draw, f'Lap {lap}', 20, 14, font, x_ratio, y_ratio)
+    draw_text(draw, f'Lap {lap}', 20 + 20, 14 + 20 + 100, font, x_ratio, y_ratio)
     # show lap time
     lap_datetime = row[COL_NAME_LAP_DATETIME]
     str_mmssms_laptime = f'{lap_datetime.minute}:{lap_datetime.second:02}.{lap_datetime.microsecond // 1000:03}'
-    draw_text(draw, str_mmssms_laptime, 20, 14 + 30, font, x_ratio, y_ratio)
+    draw_text(draw, str_mmssms_laptime, 20 + 20, 14 + 30 + 20 + 100, font, x_ratio, y_ratio)
 
 
 def draw_left_bottom(draw, font, row,
@@ -75,14 +75,14 @@ def draw_right_bottom(draw, font, power_level_max, power_level_min, row, x_ratio
     # draw power level
     power_level = row[COL_NAME_POWER_LEVEL]
     draw_zf_bar(draw, power_level, power_level_min, power_level_max,
-                1170, 860, (0, 255, 0), x_ratio, y_ratio)
-    draw_text(draw, f'{power_level:.1f} kw', 1170, 860 + 25, font, x_ratio, y_ratio,
+                1150, 860, (0, 255, 0), x_ratio, y_ratio)
+    draw_text(draw, f'{power_level:.1f} kw', 1150, 860 + 25, font, x_ratio, y_ratio,
               size_ratio,
               70, AnchorHorizontal.CENTER)
     top_progress_rect = 760
     # draw throttle rect
-    draw_progress_rect(draw, row[COL_NAME_THROTTLE] / 100, 1240, top_progress_rect, (0, 255, 0), x_ratio, y_ratio)
+    draw_progress_rect(draw, row[COL_NAME_THROTTLE] / 100, 1220, top_progress_rect, (0, 255, 0), x_ratio, y_ratio)
     # draw brake rect
-    draw_progress_rect(draw, row[COL_NAME_BRAKE] / 100, 1220, top_progress_rect, (255, 0, 0), x_ratio, y_ratio)
+    draw_progress_rect(draw, row[COL_NAME_BRAKE] / 100, 1200, top_progress_rect, (255, 0, 0), x_ratio, y_ratio)
     # draw steering wheel
-    draw_steering_wheel(draw, row[COL_NAME_STEER_ANGLE], 1170, top_progress_rect + 40, x_ratio, y_ratio, size_ratio)
+    draw_steering_wheel(draw, row[COL_NAME_STEER_ANGLE], 1150, top_progress_rect + 40, x_ratio, y_ratio, size_ratio)
