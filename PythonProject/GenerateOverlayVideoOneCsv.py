05c5c5cfc52ab3936483cba796c02f63d1fe1af7
wzj998 <>
2024-01-29 15:02:41 +0800

    show time delta overlays using one csv

--- PythonProject/GenerateOverlayVideoOneCsv.py
+++ PythonProject/GenerateOverlayVideoOneCsv.py
@@ -2,18 +2,29 @@ import numpy as np
 import pandas as pd
 import imageio
 
-from AboutDraw import OverlayVideoTool
-from Structures import ContinusLaps
+from AboutDraw import OverlayVideoTool, CurveDrawTool
+from Structures import ContinusLaps, Lap
 
 
 def main():
     csv_path = '../SampleData/telemetry-v1-2023-10-28-16_25_58.csv'
     out_video_path = '../SampleOut/overlay_video.mp4'
 
-    continus_lap = ContinusLaps.ContinusLaps(pd.read_csv(csv_path), 29)
-    # generate overlay video, background is green
-    img_paths = OverlayVideoTool.generate_overlay_video_img_paths(continus_lap, 0, 1280, 960,
-                                                                  740, 750)
+    continus_laps = ContinusLaps.ContinusLaps(pd.read_csv(csv_path), 29)
+    laps_compare = [
+        Lap.Lap(continus_laps, 0, list(continus_laps.validlap_times_dict_sorted.keys())[0]),
+        Lap.Lap(continus_laps, 1, list(continus_laps.validlap_times_dict_sorted.keys())[1])
+    ]
+    # 第二快圈作为参考圈
+    lap_checkpoints = laps_compare[1]
+    CurveDrawTool.add_col_x_m_y_m_by_lap_checkpoints(laps_compare, lap_checkpoints)
+    # we use first lap in laps_2_compare to generate checkpoints
+    df_checkpoints_lap = CurveDrawTool.get_df_checkpoints_lap(lap_checkpoints)
+    CurveDrawTool.process_laps_for_x_dist([True] * len(laps_compare), df_checkpoints_lap, laps_compare)
+
+    # generate overlay video, background is purple
+    img_paths = OverlayVideoTool.generate_overlay_video_img_paths(continus_laps, 0, 1280, 960,
+                                                                  770, 780, laps_compare)
     # save overlay video using ImageIO
     writer = imageio.get_writer(out_video_path, fps=60, macro_block_size=None)
     for i_img_path in range(len(img_paths)):
--- PythonProject/GenerateOverlayVideoTwoCsv.py
+++ PythonProject/GenerateOverlayVideoTwoCsv.py
@@ -7,11 +7,10 @@ from Structures import ContinusLaps, Lap
 
 
 def main():
-    csv_paths = '../SampleData/telemetry-v1-2024-01-14-16_49_48.csv'
-    out_video_path = '../SampleOut/overlay_video.mp4'
-
     csv_paths = ['../SampleData/telemetry-v1-2024-01-28-13_15_43.csv',
                  '../SampleData/telemetry-v1-2024-01-28-14_44_07.csv']
+    out_video_path = '../SampleOut/overlay_video.mp4'
+
     continus_lapss = [ContinusLaps.ContinusLaps(pd.read_csv(csv_path), 29) for csv_path in csv_paths]
     for i_continus_laps, continus_laps in enumerate(continus_lapss):
         print('---continus_laps', i_continus_laps, '---')
@@ -20,20 +19,20 @@ def main():
         print('validlap_times_dict_sorted:',
               ContinusLaps.get_dict_time_str_2_show(continus_laps.validlap_times_dict_sorted))
 
-    # 两个csv文件各自的最快圈对比
-    laps_2_compare = [
+    # 两个csv文件各自的最快圈对比，第一个csv文件的最快圈作为参考圈
+    laps_compare = [
         Lap.Lap(continus_lapss[0], 0, list(continus_lapss[0].validlap_times_dict_sorted.keys())[0]),
         Lap.Lap(continus_lapss[1], 1, list(continus_lapss[1].validlap_times_dict_sorted.keys())[0])
     ]
-    lap_checkpoints = laps_2_compare[0]
-    CurveDrawTool.add_col_x_m_y_m_by_lap_checkpoints(laps_2_compare, lap_checkpoints)
-    # we use first lap in laps to generate checkpoints
+    lap_checkpoints = laps_compare[0]
+    CurveDrawTool.add_col_x_m_y_m_by_lap_checkpoints(laps_compare, lap_checkpoints)
+    # we use first lap in laps_2_compare to generate checkpoints
     df_checkpoints_lap = CurveDrawTool.get_df_checkpoints_lap(lap_checkpoints)
-    CurveDrawTool.process_laps_for_x_dist([True, True], df_checkpoints_lap, laps_2_compare)
+    CurveDrawTool.process_laps_for_x_dist([True] * len(laps_compare), df_checkpoints_lap, laps_compare)
 
-    # generate overlay video, background is green
+    # generate overlay video, background is purple
     img_paths = OverlayVideoTool.generate_overlay_video_img_paths(continus_lapss[1], 1, 1920, 1080,
-                                                                  None, None, laps_2_compare)
+                                                                  None, None, laps_compare)
     # save overlay video using ImageIO
     writer = imageio.get_writer(out_video_path, fps=60, macro_block_size=None)
     for i_img_path in range(len(img_paths)):
